fc28b291ab193b83650eea4325110eef9343d374
mysr3809 <myasar0938@gmail.com>
2022-12-26 13:36:04 +0100

    implementations

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 const cellElements = document.querySelectorAll("[data-cell]");
-let circleTurn
+const boardEl = document.getElementById("board");
+let circleTurn;
 
-cellElements.forEach(cell => {
-  cell.addEventListener("click", handleClick, { once: true })
-});
+startGame();
 
-function handleClick() {
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach(cell => {
+    cell.addEventListener("click", handleClick, { once: true })
+  });
+  setHoverFeature();
+}
+
+function handleClick(e) {
   const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+
+
+  swapTurn()
+  setHoverFeature();
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurn() {
+  circleTurn = !circleTurn
+}
+
+function setHoverFeature() {
+  boardEl.classList.remove(X_CLASS);
+  boardEl.classList.remove(CIRCLE_CLASS);
+  if (circleTurn) {
+    boardEl.classList.add(CIRCLE_CLASS);
+  } else {
+    boardEl.classList.add(X_CLASS);
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combinations => {
+
+  })
 }
 
 
